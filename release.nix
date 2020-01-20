c6282a22343f2e1d71b5cbc710fcc4772e61591f
John Ericson <git@JohnEricson.me>
2020-01-20 13:15:22 -0500

    Build on darwin too

--- release.nix
+++ release.nix
@@ -1,5 +1,20 @@
-{ p ? import ./reflex-platform {}
+{ reflex-platform-fun ? import ./reflex-platform
 }:
+
 let
-  inherit (p.nixpkgs) lib;
-in p.ghc.callCabal2nix "reflex-process" ./. {}
+  native-reflex-platform = reflex-platform-fun {};
+  inherit (native-reflex-platform.nixpkgs) lib;
+  systems = [
+    "x86_64-linux"
+    "x86_64-darwin"
+  ];
+
+  perPlatform = lib.genAttrs systems (system: let
+    reflex-platform = reflex-platform-fun { inherit system; };
+  in reflex-platform.ghc.callCabal2nix "reflex-process" ./. {});
+
+in perPlatform // {
+  cache = native-reflex-platform.pinBuildInputs
+    "reflex-platform-everywhere"
+    (builtins.attrValues perPlatform);
+}
